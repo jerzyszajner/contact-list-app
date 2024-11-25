1461e4f432f6b114c8b33e427c74d4e5a0d61c16
jerzy szajner <jerzy.szajner@gmail.com>
2024-11-25 20:26:27 +0100

    implement render function

--- src/js/app.js
+++ src/js/app.js
@@ -4,10 +4,15 @@ const lastnameInput = document.querySelector("[name='lastname']");
 const phoneInput = document.querySelector("[name='phone-number']");
 const addressInput = document.querySelector("[name='address']");
 const submitButton = form.querySelector(".form__submit-button");
+const contactList = document.querySelector(".contacts__list");
 
 //DECLARING VARIABLES
 const contacts = JSON.parse(localStorage.getItem("contacts")) || [];
 
+// RENDERING CONTACTS WHEN THE PAGE FIRST LOADED
+document.addEventListener("DOMContentLoaded", () => renderContacts(contacts));
+
+// FUNCTION FOR ADDING CONTACTS TO THE LIST
 const addContacts = (e) => {
   e.preventDefault();
   const contact = {
@@ -19,11 +24,56 @@ const addContacts = (e) => {
   };
   contacts.push(contact);
   storeContacts(contacts);
+  renderContacts(contacts);
   console.log(contacts);
 };
 
-const storeContacts = (contactArray) => {
-  localStorage.setItem("contacts", JSON.stringify(contactArray));
+// FUNCTION FOR THE STORING CONTACTS IN LOCAL STORAGE
+const storeContacts = (contactsArray) => {
+  localStorage.setItem("contacts", JSON.stringify(contactsArray));
+};
+
+// FUNCTION FOR RENDERING THE CONTACTS ON THE DOM
+const renderContacts = (contactsArray) => {
+  contactList.textContent = "";
+  contactsArray.forEach((contact, index) => {
+    // CREATE ELEMENTS
+    const contactItem = document.createElement("li");
+    const contactListNumber = document.createElement("span");
+    const contactFullname = document.createElement("span");
+    const contactPhoneNumber = document.createElement("span");
+    const contactAddress = document.createElement("span");
+    const contactTools = document.createElement("span");
+    const deleteButton = document.createElement("button");
+    const editButton = document.createElement("button");
+
+    // APPEND ELEMENTS
+    contactList.append(contactItem);
+    contactItem.append(
+      contactListNumber,
+      contactFullname,
+      contactPhoneNumber,
+      contactAddress,
+      contactTools
+    );
+    contactTools.append(deleteButton, editButton);
+
+    // INSERT CONTENT INTO ELEMENTS
+    contactListNumber.textContent = `${index + 1}.`;
+    contactFullname.textContent = `${contact.contactFirstname} ${contact.contactLastname}`;
+    contactPhoneNumber.textContent = contact.contactPhoneNumber;
+    contactAddress.textContent = contact.contactAddress;
+    deleteButton.innerHTML = "<i class='fa-solid fa-trash-can'></i>";
+    editButton.innerHTML = "<i class='fa-solid fa-pencil'></i>";
+
+    //ADDING CLASS TO ELEMENTS
+    contactItem.classList.add("contacts-item");
+    contactListNumber.classList.add("contacts-item__list-number");
+    contactFullname.classList.add("contacts-item__fullname");
+    contactPhoneNumber.classList.add("contacts-item__phone");
+    contactAddress.classList.add("contacts-item__address");
+    contactTools.classList.add("contacts-item__controls");
+  });
 };
 
 // ADD EVENT LISTENER TO THE CONTACTS
