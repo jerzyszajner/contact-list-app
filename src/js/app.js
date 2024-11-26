b281648b0a82bff46c826dc51be1248eeddf73f1
jerzy szajner <jerzy.szajner@gmail.com>
2024-11-26 12:08:26 +0100

    implement edit functionality with dynamic button text and contact highlighting

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+// SELECTING ELEMENTS
 const form = document.querySelector(".contact-app__form");
 const firstnameInput = document.querySelector("[name='firstname']");
 const lastnameInput = document.querySelector("[name='lastname']");
@@ -5,9 +6,13 @@ const phoneInput = document.querySelector("[name='phone-number']");
 const addressInput = document.querySelector("[name='address']");
 const submitButton = form.querySelector(".form__submit-button");
 const contactList = document.querySelector(".contacts__list");
+const searchInput = document.querySelector(".search__input");
+const searchOption = document.querySelector(".search__filter");
 
 //DECLARING VARIABLES
 const contacts = JSON.parse(localStorage.getItem("contacts")) || [];
+
+let editContactId = null;
 
 // RENDERING CONTACTS WHEN THE PAGE FIRST LOADED
 document.addEventListener("DOMContentLoaded", () => renderContacts(contacts));
@@ -22,10 +27,22 @@ const addContacts = (e) => {
     contactPhoneNumber: phoneInput.value,
     contactAddress: addressInput.value,
   };
-  contacts.push(contact);
+
+  if (editContactId) {
+    const contactIndex = contacts.findIndex(
+      (contact) => contact.id === editContactId
+    );
+    if (contactIndex !== -1) {
+      contacts[contactIndex] = contact;
+    }
+    editContactId = null;
+  } else {
+    contacts.push(contact);
+  }
+
   storeContacts(contacts);
   renderContacts(contacts);
-  console.log(contacts);
+  form.reset();
 };
 
 // FUNCTION FOR THE STORING CONTACTS IN LOCAL STORAGE
@@ -35,10 +52,27 @@ const storeContacts = (contactsArray) => {
 
 // FUNCTION FOR DELETING CONTACTS FORM THE LIST
 const deleteContacts = (id) => {
-  const contacts = JSON.parse(localStorage.getItem("contacts"));
   const remainingContacts = contacts.filter((contact) => contact.id !== id);
   storeContacts(remainingContacts);
   renderContacts(remainingContacts);
+};
+
+// FUNCTION FOR EDITING CONTACTS
+const editContacts = (e, id) => {
+  const contactToEdit = contacts.find((contact) => contact.id === id);
+  const contactRow = e.target.closest(".contacts-item");
+  if (contactToEdit) {
+    firstnameInput.value = contactToEdit.contactFirstname;
+    lastnameInput.value = contactToEdit.contactLastname;
+    phoneInput.value = contactToEdit.contactPhoneNumber;
+    addressInput.value = contactToEdit.contactAddress;
+    editContactId = id;
+    submitButton.textContent = "Update Contact";
+    contactRow.style.backgroundColor = "#FFFED3";
+  } else {
+    submitButton.textContent = "Add Contact";
+    contactRow.style.backgroundColor = "#d4f6ff";
+  }
 };
 
 // FUNCTION FOR RENDERING THE CONTACTS ON THE DOM
@@ -84,8 +118,27 @@ const renderContacts = (contactsArray) => {
 
     // ADD EVENT LISTNERS TO DELETE AND EDIT BUTTON
     deleteButton.addEventListener("click", () => deleteContacts(contact.id));
+    editButton.addEventListener("click", (e) => editContacts(e, contact.id));
   });
 };
 
 // ADD EVENT LISTENER TO THE CONTACTS
 form.addEventListener("submit", addContacts);
+
+// ADD EVENTLISTENER TO THE SEARCH INPUT AND MAKE IT TO
+searchInput.addEventListener("input", (e) => {
+  const searchQuery = e.target.value.toLowerCase();
+  const searchOptionValue = searchOption.value;
+  const filteredArray = contacts.filter((contact) => {
+    if (searchOptionValue === "firstname") {
+      return contact.contactFirstname.toLowerCase().startsWith(searchQuery);
+    } else if (searchOptionValue === "lastname") {
+      return contact.contactLastname.toLowerCase().startsWith(searchQuery);
+    } else if (searchOptionValue === "phone") {
+      return contact.contactPhoneNumber.toLowerCase().startsWith(searchQuery);
+    } else {
+      return;
+    }
+  });
+  renderContacts(filteredArray);
+});
